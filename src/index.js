9e6b19e49a7ecf3463eddd55d3373571c023ddb4
Jon Preece <jonpreece@hotmail.co.uk>
2019-04-04 19:13:51 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const change = msg => {
+  document.querySelector("body").innerText = msg;
+};
+
+document.querySelector("body").innerText = "Hello, World!";
+
+setTimeout(() => {
+  change("Deferred hello world!");
+}, 3000);
